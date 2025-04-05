69856c21e906504a0e02ca3f2017b08aa2874f9c
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-04-06 02:43:04 +0900

    secret

--- app/modules/config.py
+++ app/modules/config.py
@@ -11,14 +11,13 @@ def get_db_url():
         # ローカルで DATABASE_URL が定義されていれば、それを使う
     local_url = os.getenv("DATABASE_URL")
     if local_url:
-        return local_url
-    secret_name = "AuroraAuroraSecretF26D12ED-oLCM3z1Q5Oxo"
-    print("secret_name",secret_name)
-    client = boto3.client("secretsmanager")
-    response = client.get_secret_value(SecretId=secret_name)
-    secret = json.loads(response["SecretString"])
-    print("secret",secret)
+        return local_url    
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
     return (
-        f"postgresql://{secret['username']}:{urllib.parse.quote_plus(secret['password'])}"
-        f"@{secret['host']}:{secret['port']}/{secret['dbname']}"
+        f"postgresql://{DB_USERNAME}:{urllib.parse.quote_plus(DB_PASSWORD)}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
